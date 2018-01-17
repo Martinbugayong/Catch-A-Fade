f0b023b3b96b1ea1d092a4185485df7238c7bc78
Martin Bugayong <martinbugayong@Martins-MacBook-Pro.local>
2018-01-17 09:29:27 -0800

    delete unnecessary files

--- app/controllers/book_controller.rb
+++
@@ -1,4 +0,0 @@
-class BookController < 
-    # put unauthorized until loged in actions here 
-    before_action :authorize, except: [:index, :show]
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117011206) do
+ActiveRecord::Schema.define(version: 20180117172558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180117011206) do
     t.text "details"
     t.string "date"
     t.string "time"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20180117011206) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "users"
 end
